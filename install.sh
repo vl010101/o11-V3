e2ea3832ed17e51b0a8873b1e53b1b25cf89ace2
sibuk76 <160167068+sibuk76@users.noreply.github.com>
2025-08-25 11:27:50 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+apt update
+apt install unrar -y
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar -xf ffmpeg-release-amd64-static.tar.xz
+cd ffmpeg-7.0.2-amd64-static/
+mv ffmpeg /usr/local/bin/
+mv ffprobe /usr/local/bin/
+cd /root
+wget https://github.com/sibuk76/o11-v3/raw/main/o11.rar
+unrar x o11.rar /home
+chmod -R +x /home/o11/
+wget https://raw.githubusercontent.com/sibuk76/o11-v3/main/o11.service
+chmod +x o11.service
+mv ./o11.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable o11.service
+systemctl start o11.service
+
+echo "Installation Complete"
+echo "Please login http://YOURIP:7070"
+echo "USERNAME/PASSWORD admin admin"
